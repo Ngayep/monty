4f614ea570f55bb14c8f1e10b4b1ce9d997ba8b4
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-14 13:35:00 -0800

    pop nop pint

--- pall.c
+++ pall.c
@@ -41,3 +41,46 @@ void pint(stack_t **stack, unsigned int lnumber)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * pop - removes the last inserted element
+ *
+ * @lnumber: number of lines in stack
+ * @stack: pointer to the pointer to the head
+ * Return: void
+ */
+
+void pop(stack_t **stack, unsigned int lnumber)
+{
+	stack_t *last;
+
+	if (stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", lnumber);
+		exit(EXIT_FAILURE);
+	}
+	else if (*stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", lnumber);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		last = *stack;
+		*stack = (*stack)->next;
+		free(last);
+	}
+}
+
+/**
+ * nop - does nothing
+ * @stack: pointer to pointer to the stack
+ * @lnumber: number of lines in the stack
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int lnumber)
+{
+	(void) stack;
+	(void) lnumber;
+}
