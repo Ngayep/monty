c5d906374ec1c5b7002a2073e76d24a58f6bc3c7
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-14 12:23:18 -0800

    push pall

--- push.c
+++ push.c
@@ -1,33 +1,59 @@
 #include "monty.h"
 /**
  * push - add node to the stack
- * @head: stack head
- * @number: stackline number
+ * @head: pointer to  stack head
+ * @line: pointer to line
+ * @lnumber: stackline number
  * Return: no return
  */
 
-void push(stack_t **stack, unsigned int number)
+void push(stack_t **head, char *line, unsigned int lnumber)
 {
-	stack_t *newNode = malloc(sizeof(stack_t));
+	stack_t *newstack = NULL;
+	unsigned int i;
 
-	if (!newNode)
+	newstack = malloc(sizeof(stack_t));
+	if (newstack == NULL)
 	{
-		perror("Memory allocation failed");
+		fprintf(stderr, "L%u: usage: push integer\n", lnumber);
 		exit(EXIT_FAILURE);
 	}
 
-	newNode->n = number;
-	newNode->prev = NULL;
-
-	if (*stack != NULL)
+	if (line == NULL)
 	{
-		newNode->next = *stack;
-		(*stack)->prev = newNode;
+		fprintf(stderr, "L%u: usage: push integer\n", lnumber);
+		exit(EXIT_FAILURE);
 	}
-	else
+	for (i = 0; line[i]; i++)
 	{
-		newNode->next = NULL;
+		if (line[0] == '-' && i == 0)
+			continue;
+		if (line[i] < 48 || line[i] > 57)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", lnumber);
+			exit(EXIT_FAILURE);
+		}
 	}
+	newstack->n = atoi(line);
+	newstack->prev = NULL;
+	newstack->next = NULL;
 
-	*stack = newNode;
+	if (*head != NULL)
+	{
+		newstack->next = *head;
+		(*head)->prev = newstack;
+	}
+	*head = newstack;
+}
+
+void free_stack(stack_t *stack)
+{
+	stack_t *current;
+
+	while (stack != NULL)
+	{
+		current = stack;
+		stack = stack->next;
+		free(current);
+	}
 }
