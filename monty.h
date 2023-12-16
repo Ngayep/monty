753ad125a00563d752fb58bb3a9445f858dcc4f1
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-15 23:27:48 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -7,8 +7,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdarg.h>
-#include <errno.h>
 
 
 /**
@@ -41,6 +39,62 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct args_s - structure of arguments from main
+ * @av: name of the file from the command line
+ * @ac: number of arguments from main
+ * @line_number: number of the current line in the file
+ *
+ * Description: arguments passed to main from the command line
+ * used in different functions, organized in a struct for clarity
+ */
+typedef struct args_s
+{
+	char *av;
+	int ac;
+	unsigned int line_number;
+} args_t;
+
+/**
+ * struct data_s - extern data to access inside functions
+ * @line: line from the file
+ * @words: parsed line
+ * @stack: pointer to the stack
+ * @fptr: file pointer
+ * @qflag: flag for queue or stack
+ */
+typedef struct data_s
+{
+	char *line;
+	char **words;
+	stack_t *stack;
+	FILE *fptr;
+	int qflag;
+} data_t;
+
+typedef stack_t dlistint_t;
+
+extern data_t data;
+
+#define DATA_INIT {NULL, NULL, NULL, NULL, 0}
+
+#define USAGE "USAGE: monty file\n"
+#define FILE_ERROR "Error: Can't open file %s\n"
+#define UNKNOWN "L%u: unknown instruction %s\n"
+#define MALLOC_FAIL "Error: malloc failed\n"
+#define PUSH_FAIL "L%u: usage: push integer\n"
+#define PINT_FAIL "L%u: can't pint, stack empty\n"
+#define POP_FAIL "L%u: can't pop an empty stack\n"
+#define SWAP_FAIL "L%u: can't swap, stack too short\n"
+#define ADD_FAIL "L%u: can't add, stack too short\n"
+#define SUB_FAIL "L%u: can't sub, stack too short\n"
+#define DIV_FAIL "L%u: can't div, stack too short\n"
+#define DIV_ZERO "L%u: division by zero\n"
+#define MUL_FAIL "L%u: can't mul, stack too short\n"
+#define MOD_FAIL "L%u: can't mod, stack too short\n"
+#define PCHAR_FAIL "L%u: can't pchar, stack empty\n"
+#define PCHAR_RANGE "L%u: can't pchar, value out of range\n"
+
 void push(stack_t **stack, unsigned int line_number);
 void (*get_func(char **parsed))(stack_t **, unsigned int);
 void pint(stack_t **stack, unsigned int line_number);
@@ -48,13 +102,18 @@ void pall(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int lnumber);
 void pop(stack_t **stack, unsigned int lnumber);
 void swap(stack_t **stack, unsigned int lnumber);
+void queue(stack_t **stack, unsigned int line_number);
+void stack_handle(stack_t **stack, unsigned int line_number);
 
 
+void free_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 void free_all(int allz);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif
