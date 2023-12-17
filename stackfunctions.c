e741fcd9dff38fe5b8801e02a0286f3e6c48f6c6
Jessica Ngayep <courtesyofjess@gmail.com>
2023-12-17 07:37:38 -0800

    add swap

--- stackfunctions.c
+++ stackfunctions.c
@@ -23,3 +23,63 @@ void queue(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	data.qflag = 1;
 }
+
+/**
+ * add - handles the add instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int s = 0;
+	stack_t *node = NULL;
+	stack_t *node_0 = get_dnodeint_at_index(*stack, 0);
+	stack_t *node_1 = get_dnodeint_at_index(*stack, 1);
+
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, ADD_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	s = node_0->n + node_1->n;
+	delete_dnodeint_at_index(stack, 0);
+	delete_dnodeint_at_index(stack, 0);
+	node = add_dnodeint(stack, s);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * swap - handles the swap instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack, *node = NULL;
+	int num;
+
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, SWAP_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = get_dnodeint_at_index(*stack, 0);
+	num = tmp->n;
+	delete_dnodeint_at_index(stack, 0);
+	node = insert_dnodeint_at_index(stack, 1, num);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+}
